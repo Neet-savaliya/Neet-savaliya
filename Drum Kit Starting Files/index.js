34a229cbf0886b1c0664b6b912f58dc6eb324d9c
neetsavaliya <neetsavaliya03@gmail.com>
2023-06-09 11:41:43 +0530

    Functionality

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,3 +1,24 @@
-"use strict"
+"use strict";
 
+const drums = document.querySelectorAll(".drum");
+const audios = [
+  "./sounds/tom-1.mp3",
+  "./sounds/tom-2.mp3",
+  "./sounds/tom-3.mp3",
+  "./sounds/tom-4.mp3",
+  "./sounds/kick-bass.mp3",
+  "./sounds/snare.mp3",
+  "./sounds/crash.mp3",
+];
 
+drums.forEach((element, i) => {
+  element.addEventListener("click", () => {
+    new Audio(audios[i]).play();
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key.toLowerCase() == element.innerText.toLowerCase()) {
+      new Audio(audios[i]).play();
+    }
+  });
+});
