55be42311adf8ae3ec1a7952677c2cafd967c5dd
neetsavaliya <neetsavaliya03@gmail.com>
2023-06-09 12:27:40 +0530

    Adding animation to button

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -11,14 +11,23 @@ const audios = [
   "./sounds/crash.mp3",
 ];
 
+const animateButton = (i) => {
+  drums[i].classList.add("pressed");
+  setTimeout(() => {
+    drums[i].classList.remove("pressed");
+  }, 100);
+};
+
 drums.forEach((element, i) => {
   element.addEventListener("click", () => {
     new Audio(audios[i]).play();
+    animateButton(i);
   });
 
   window.addEventListener("keydown", (e) => {
     if (e.key.toLowerCase() == element.innerText.toLowerCase()) {
       new Audio(audios[i]).play();
+      animateButton(i);
     }
   });
 });
